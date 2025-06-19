fb57c60b98583a877e365b0e6d56d67f82332b08
Fidaa2002Shwahna <programmerfidaa@gmail.com>
2025-06-19 18:52:23 +0300

    repair git.visit

--- cypress/e2e/task-1.cy.js
+++ cypress/e2e/task-1.cy.js
@@ -2,63 +2,56 @@
 
 
 
-// I use one scenario to write test cases , I depend on "it.only" to execute it 
+// I use one scenario to write test cases 
 describe('Login Page Functionality', () => {
+    beforeEach(() => {
+        cy.visit("https://demo.productionready.io/#/register");
+    });
     it('verify that the logo is appearing correctly', () => {
-        cy.visit("/");
         cy.get(".container").contains("conduit")
     });
 
     it('verify that the home link is appearing correctly', () => {
-        cy.visit("/");
         cy.get('[show-authed="false"]').find("li").first()
     })
 
     it('verify that the Sign up link is appearing correctly', () => {
-        cy.visit("/");
         cy.get('[show-authed="false"]').find("li").last()
     })
 
     it('verify that the Sign up heading is appearing correctly', () => {
-        cy.visit("/");
         cy.get("h1")
     })
 
     it('verify that the Have an account? link is appearing correctly', () => {
-        cy.visit("/");
-        cy.contains("Have an account?").click()
+        cy.contains("Have an account?")
     })
 
     it('verify that the username input is appearing correctly', () => {
-        cy.visit("/")
         cy.get("form.ng-pristine").find(".form-group").first()
     })
 
     it('verify that the email input is appearing correctly', () => {
-        cy.visit("/")
         cy.get("form.ng-pristine").find(".form-group").eq(1)
     })
 
     it('verify that the password input is appearing correctly', () => {
-        cy.visit("/");
         cy.get("form.ng-pristine").find(".form-group").last()
     })
 
     it('verify that the Sign Up button is appearing correctly', () => {
-        cy.visit("/")
         cy.get("button")
     })
 
 
     it('verify that the conduit link is appearing correctly', () => {
-        cy.visit("/")
         cy.get(".logo-font")
     })
 
 
     it('verify that the copyright is appearing correctly', () => {
-        cy.visit("/")
         cy.get("span")
     })
 
 });
+
